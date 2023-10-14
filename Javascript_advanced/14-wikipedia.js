7eb76453ee263fdedf2f1bd55971dac178054d9a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-10-14 12:12:14 -0400

    Fix 14th exercise

--- Javascript_advanced/14-wikipedia.js
+++ Javascript_advanced/14-wikipedia.js
@@ -1,13 +1,19 @@
-function createElement() {
+function createElement(data) {
     const p = document.createElement('p');
 
-    p.innerText = this.responseText;
+    p.innerText = data;
     document.body.appendChild(p);
 }
 
 function queryWikipedia(callback) {
     const stackoverflowRequest = new XMLHttpRequest();
-    stackoverflowRequest.addEventListener('load', callback);
+    stackoverflowRequest.onload = function () {
+        callback(
+            JSON.parse(
+                stackoverflowRequest.responseText
+            ).query.pages[21721040].extract
+        );
+    }
     stackoverflowRequest.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*');
     stackoverflowRequest.send();
 }
